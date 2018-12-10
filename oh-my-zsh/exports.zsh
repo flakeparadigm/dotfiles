cabe787fc86c457dd90f3fc7bdc73f52d21a93a3
Tyler Nienhouse <flakeparadigm@gmail.com>
2018-12-10 23:35:59 +1100

    ssh w/gpg

--- oh-my-zsh/exports.zsh
+++ oh-my-zsh/exports.zsh
@@ -1,4 +1,12 @@
 # export PATH=~/Library/Python/2.7/bin:$PATH:/Users/tyler/salt-developer/scripts
 # export PATH=~/go/bin:$PATH
+
 export GPG_TTY=$(tty)
 
+if [ "$(uname)" = 'Darwin' ]; then
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+    gpgconf --launch gpg-agent > /dev/null 2>&1
+elif [ "$(uname)" = 'Linux' ]; then
+    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+    gpg-connect-agent updatestartuptty /bye > /dev/null 2>&1
+fi
